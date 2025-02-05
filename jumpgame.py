8acb6129c62e588c4d46fc7da80f6fc2ffd19e4b
Vikash Yadav <149170851+Vikash013@users.noreply.github.com>
2025-02-05 13:29:41 +0530

    Create jumpgame.py

---
+++ jumpgame.py
@@ -0,0 +1,14 @@
+class Solution:
+    def jump(self, nums: List[int]) -> int:
+        smallest = 0
+        n = len(nums)
+        far = end = 0
+
+        for i in range(n-1):
+            far = max(far, i+nums[i])
+
+            if i == end:
+                smallest += 1
+                end = far
+
+        return smallest
